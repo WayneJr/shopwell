052e6234e55751a0b08c941eb19ce8793201ef49
streetCoder <streetcoder99@gmail.com>
2023-02-14 15:24:28 +0100

    feat(): frontend changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
-import { getUserInfo } from '../localStorage.js';
-import { parseRequestUrl } from '../utils.js';
+import { getUserInfo } from "../localStorage.js";
+import { parseRequestUrl } from "../utils.js";
 
 const Header = {
   render: async () => {
@@ -8,12 +8,20 @@ const Header = {
       <div class="brand">
         <button id="aside-open-button">
           &#9776;
-        </button>
-        <a href="/#/" >UI Shopwell</a>
+        </button>       
+        <div class="logo-div">
+        <a href="/#/" >
+        <img class="fit-picture"
+          src="/images/logo.png"
+          alt="UI Logo"> 
+          
+        UI Shopwell
+        </a>
+        </div>
       </div>
       <div class="search">
         <form class="search-form"  id="search-form">
-          <input type="text" name="q" id="q" value="${value || ''}" /> 
+          <input type="text" name="q" id="q" value="${value || ""}" /> 
           <button type="submit"><i class="fa fa-search"></i></button>
         </form>        
       </div>
@@ -27,27 +35,27 @@ const Header = {
     const { name, isAdmin } = getUserInfo();
     if (name) {
       if (isAdmin) {
-        document.getElementById('header-links').innerHTML = `
+        document.getElementById("header-links").innerHTML = `
         <a href="/#/profile">${name}</a>
         <a href="/#/cart">Cart</a>
         <a href="/#/dashboard">Dashboard</a> `;
       } else {
-        document.getElementById('header-links').innerHTML = `
+        document.getElementById("header-links").innerHTML = `
         <a href="/#/profile">${name}</a>
         <a href="/#/cart">Cart</a>`;
       }
     }
     document
-      .getElementById('search-form')
-      .addEventListener('submit', async (e) => {
+      .getElementById("search-form")
+      .addEventListener("submit", async (e) => {
         e.preventDefault();
-        const searchKeyword = document.getElementById('q').value;
+        const searchKeyword = document.getElementById("q").value;
         document.location.hash = `/?q=${searchKeyword}`;
       });
     document
-      .getElementById('aside-open-button')
-      .addEventListener('click', async () => {
-        document.getElementById('aside_container').classList.add('open');
+      .getElementById("aside-open-button")
+      .addEventListener("click", async () => {
+        document.getElementById("aside_container").classList.add("open");
       });
   },
 };
